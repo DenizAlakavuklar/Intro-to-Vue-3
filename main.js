9d8a896f2a69ff1d74de6b1e00d8777b5f3b73d4
DenizAlakavuklar <deniz.kovanci@gmail.com>
2023-05-27 16:48:43 +0200

    mvp is done

--- main.js
+++ main.js
@@ -1,30 +1,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
+            cart: [],
+            premium: true
         }
     },
     methods: {
-        addToCart() {
-            this.cart += 1
+        updateCart(id) {
+            this.cart.push(id)
         },
         // solution
-        removeFromCart() {
-            if (this.cart >= 1) {
-                this.cart -= 1
-            }
-        },
-        // solution
-        updateImage(variantImage) {
-            this.image = variantImage
+        removeById(id) {
+            const index = this.cart.indexOf(id)
+                if (index > -1) {
+                    this.cart.splice(index, 1)
+                }
         }
+        // solution
     }
 })
